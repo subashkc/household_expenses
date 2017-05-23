69f7f177c9a91bfe92e8dc6be610d16199a7a13b
Subash KC <subashkc1@gmail.com>
2017-05-23 14:58:28 +1000

    refactored a class component into functional component

--- app/assets/javascripts/components/amounts_box.js.jsx
+++ app/assets/javascripts/components/amounts_box.js.jsx
@@ -1,21 +1,14 @@
-class AmountsBox extends React.Component{
-
-	constructor(props) {
-		super(props)
-	}
-
-	render() {
-		return (
-			<div className="col-md-4">
-				<div className={"panel panel-"+this.props.type}>
-					<div className="panel-heading">
-						{this.props.text}
-					</div>
-					<div className="panel-body">
-						{this.props.value}
-					</div>
+const AmountsBox = (props) => {
+	return (
+		<div className="col-md-4">
+			<div className={"panel panel-"+props.type}>
+				<div className="panel-heading">
+					{props.text}
+				</div>
+				<div className="panel-body">
+					{props.value}
 				</div>
 			</div>
-		)
-	}
+		</div>
+	)
 }
